e6c832c0bcdc7a6e59e51b90c651364e23e100be
AnLorXen <anlorxen@cox.net>
2017-04-24 15:53:19 -0700

    Update eschar.js

--- public/js/eschar.js
+++ public/js/eschar.js
@@ -1,1 +1,16 @@
 
+
+var eschar = (function () {
+  "use strict";
+
+  var initModule = function (_container) {
+    eschar.chassis.initModule(_container);
+  };
+
+  return {
+    "initModule": initModule
+  };
+}());
+
+
+
